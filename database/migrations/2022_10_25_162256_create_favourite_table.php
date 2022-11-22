cf32cfeb74a8b8c97e84896e17b80480c8b064d7
bjts0807 <bjts0807@gmail.com>
2022-11-21 22:32:54 -0500

    first commit

---
+++ database/migrations/2022_10_25_162256_create_favourite_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFavouriteTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favourite', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_usuario')->unsigned();
+            $table->foreign('id_usuario')->references('id')->on('users');
+            $table->string('ref_api');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favourite');
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Http\Controllers\FavouriteController;
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::prefix('users')->group(function () {
+    Route::get('/', [UserController::class, 'index']);
+    Route::post('/store', [UserController::class, 'store']);
+    Route::put('/update', [UserController::class, 'update']);
+    Route::get('/show/{id}', [UserController::class, 'show']);
+});
+
+Route::prefix('favourite')->group(function () {
+    Route::get('/', [FavouriteController::class, 'index']);
+    Route::post('/store', [FavouriteController::class, 'store']);
+    Route::put('/update', [FavouriteController::class, 'update']);
+    Route::delete('/destroy', [FavouriteController::class, 'destroy']);
+    Route::get('/show/{id}', [FavouriteController::class, 'show']);
+});
+
